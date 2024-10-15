a6a16b38e1d2f32960880ba0a7a8dc0f838f82f9
habbaza <habbazahubbal@gmail.com>
2024-10-15 13:34:47 +0700

    Edit url endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 const routes = (
   <Router>
     <Routes>
-      <Route path="/dashboard" exact element={<Home />} />
+      <Route path="/" exact element={<Home />} />
       <Route path="/login" exact element={<Login />} />
       <Route path="/signup" exact element={<SignUp />} />
+      <Route path="*" exact element={<h1>Not found</h1>} />
     </Routes>
   </Router>
 )
